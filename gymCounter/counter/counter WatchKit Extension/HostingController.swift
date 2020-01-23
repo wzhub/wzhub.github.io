b841a3644d3432770a3cf0def81d0aaaf7528126
Jone Wen <wenzh@apple.com>
2020-01-24 07:24:42 +0800

    create gymCounter App

---
+++ gymCounter/counter/counter WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  counter WatchKit Extension
+//
+//  Created by J on 2020/1/24.
+//  Copyright © 2020 J1. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
