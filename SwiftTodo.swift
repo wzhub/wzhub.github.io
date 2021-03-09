7d9adf957f6e1d8bd581dea66f6b7a227b292118
wzhub <wenzhun@hotmail.com>
2021-03-09 19:16:33 +0800

    Add file via upload
    To test if iOS GitHub can make changes to it

---
+++ SwiftTodo.swift
@@ -0,0 +1,10 @@
+struct todoListItem {
+}
+// the struct is used to add tags
+// but is there a way to filter all the items with specific tag?
+
+let swiftStudy = [
+    "create struct for Bear social App",
+    "Finish reading AP CS Swift book",
+    "reopen GitHub personal page, set a progress bar for 6-weeks project, then"
+]
