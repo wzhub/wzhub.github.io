acc607a19c4e749bdcc40ef0f6ca30a8c52108eb
wzhub <wenzhun@hotmail.com>
2024-02-18 19:30:13 +0800

    Add files via upload

--- cl.swift
+++ cl.swift
@@ -1,26 +1,27 @@
 import ArgumentParser
 @main
 struct td: ParsableCommand {
- @Argument var userInput:String
+@Argument var userInput:String
  mutating func run() throws {
-  var todoDictionary:[String:String]=[:]
-  var somedayList=["以后："]
-  var doneList=["已完成："]
   var temporaryString=""
+  var somedayList=["未来某天："]
+  var doneList=["已完成："]
+  var todoList=["待办："]
   for character in userInput{
    if character=="\n"{
     switch temporaryString{
-     case let x where x.hasPrefix("[x]"):doneList.append(temporaryString)
      case let x where x.hasPrefix("[?]"):somedayList.append(temporaryString)
-     default:todoDictionary[temporaryString]="[ ]"
+     case let x where x.hasPrefix("[x]"):doneList.append(temporaryString)
+     default:todoList.append("[ ] "+temporaryString)
     }
     temporaryString=""
    }else{temporaryString.append(character)}
   }
-  for item in (somedayList+doneList){print(item)}
-  print("待办：")
-  for (key,value) in todoDictionary{
-   print(value,key)
+  for item in (somedayList+doneList+todoList){print(item)}
+  if todoList.count>1{
+   todoList.removeFirst()
+   print("下一件：")
+   if let nextRandom=todoList.randomElement(){print(nextRandom)}
   }
  }
 }
