ba7adb041f5bc791ae4b3e56a96cf14fd4aca4a7
wzhub <wenzhun@hotmail.com>
2024-02-21 14:50:41 +0800

    Add files via upload

--- cl.swift
+++ cl.swift
@@ -1,32 +1,33 @@
 import ArgumentParser
 @main
 struct td: ParsableCommand {
- @Argument var userInput:String
+@Argument var userInput:String
 //用户输入待办事项列表，已完成的条目用“[x]”标记，以后再做的事用“[?]”标记，当日待办事项不做标记
  mutating func run() throws {
-  userInput+="\n"
-//程序执行的时候以换行符作为一个标志，用户的输入通常最后一行不带有换行标志，这里加入一个以确保最后一行不会被错过
   var temporaryString=""
-  var todoList=[""]
+  var todoList=[String]()
+//userInput一定会有内容，所以这个列表不会是空的
   var todoProgress=""
   var otherProgress=""
+  userInput+="\n"
+//程序执行的时候以换行符作为一个标志，用户的输入通常最后一行不带有换行标志，这里加入一个以确保最后一行不会被错过
   for character in userInput{
    if character=="\n"{
-    if !temporaryString.hasPrefix("[x]") && !temporaryString.hasPrefix("[?]") && !temporaryString.isEmpty{
-     todoList.append(temporaryString)
-     todoProgress+="☐"
-    }else{otherProgress+="◼︎"}
-    temporaryString=""
-   }else{
-    temporaryString.append(character)
-   }
+    if !temporaryString.isEmpty{
+     switch !temporaryString.hasPrefix("[x]") && !temporaryString.hasPrefix("[?]"){
+      case true:
+       todoList.append(temporaryString)
+       todoProgress+="☐"
+      case false:otherProgress+="◼︎"
+     }
+     temporaryString="" 
+    } 
+   }else{temporaryString.append(character)}
   }
-  print(otherProgress+todoProgress)
-//展示进度条
-  if todoList.count>1{
-   todoList.removeFirst()
-   if let randomTodoItem=todoList.randomElement(){print(randomTodoItem)}
+  let progressbar=otherProgress+todoProgress
+  if progressbar.count>2{print(progressbar)}
+//如果有超过两个条目，展示进度条。否则只有一个或两个小方块，非常像乱码
+  if let randomTodo=todoList.randomElement(){print(randomTodo)}
 //随机展示一条待办事项
-  }
  }
 }
